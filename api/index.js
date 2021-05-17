48035afd52af84058974635327ef395b25830170
plumest <plumest.chayathon@gmail.com>
2021-05-18 00:19:36 +0700

    Adding rate limit

--- api/index.js
+++ api/index.js
@@ -4,8 +4,9 @@ import helmet from "helmet";
 import logger from 'morgan';
 import express from 'express';
 import mongoose from 'mongoose';
-import routes from './routes/index.js';
 import bodyParser from "body-parser";
+import routes from './routes/index.js';
+import rateLimit from 'express-rate-limit';
 
 const app = express();
 
@@ -13,6 +14,13 @@ app.use(cors());
 app.use(helmet());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100 // limit each IP to 100 requests per windowMs
+});
+
+app.use(limiter);
 
 mongoose.connect(config.connUri, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
     .catch(error => console.log(error));
