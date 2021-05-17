c5fcd6efe73d0527e41bb9b59f80c12b89cf1a4c
plumest <plumest.chayathon@gmail.com>
2021-05-18 01:18:08 +0700

    Fix conflix

--- api/src/index.js
+++ api/src/index.js
@@ -4,11 +4,7 @@ import helmet from "helmet";
 import logger from 'morgan';
 import express from 'express';
 import mongoose from 'mongoose';
-<<<<<<< HEAD
-=======
 const load = require('loaders/index')
-import routes from './routes/index.js';
->>>>>>> a040187f516664adb4b30887f44b7f7e12c46328
 import bodyParser from "body-parser";
 import routes from './routes/index.js';
 import rateLimit from 'express-rate-limit';
@@ -16,34 +12,34 @@ import rateLimit from 'express-rate-limit';
 let app = express();
 
 async function startServer() {
-  app = await load()
-  if (config.environment !== 'production') {
-    app.use(logger('dev'));
-  }
-  app.use(cors());
-  app.use(helmet());
-  app.use(bodyParser.json());
-  app.use(bodyParser.urlencoded({extended: true}));
-  app.use('/api/v1', routes.users);
+    app = await load()
+    if (config.environment !== 'production') {
+        app.use(logger('dev'));
+    }
+    app.use(cors());
+    app.use(helmet());
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended: true}));
+    app.use('/api/v1', routes.users);
 
-<<<<<<< HEAD
-const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100 // limit each IP to 100 requests per windowMs
-});
 
-app.use(limiter);
+    const limiter = rateLimit({
+        windowMs: 15 * 60 * 1000, // 15 minutes
+        max: 100 // limit each IP to 100 requests per windowMs
+    });
 
-mongoose.connect(config.connUri, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
-    .catch(error => console.log(error));
+    app.use(limiter);
 
-if (config.environment !== 'production') {
-  app.use(logger('dev'));
-=======
-  app.listen(config.port, () => {
-    console.log(`Server is running on ${config.baseUrl}:${config.port}/`);
-  });
->>>>>>> a040187f516664adb4b30887f44b7f7e12c46328
+    mongoose.connect(config.connUri, {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true})
+        .catch(error => console.log(error));
+
+    if (config.environment !== 'production') {
+        app.use(logger('dev'));
+
+        app.listen(config.port, () => {
+            console.log(`Server is running on ${config.baseUrl}:${config.port}/`);
+        });
+    }
 }
 
 startServer();
