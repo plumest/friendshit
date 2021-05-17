9d7a6662ab8d7760f84119d9430f60ad9db9b7fb
Th <tharathorn.bu@ku.th>
2021-05-18 00:43:33 +0700

    Add loader for express and mongoose

--- api/src/index.js
+++ api/src/index.js
@@ -4,25 +4,31 @@ import helmet from "helmet";
 import logger from 'morgan';
 import express from 'express';
 import mongoose from 'mongoose';
+const load = require('loaders/index')
 import routes from './routes/index.js';
 import bodyParser from "body-parser";
 
-const app = express();
+let app = express();
 
-app.use(cors());
-app.use(helmet());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+async function startServer() {
+  app = await load()
+  if (config.environment !== 'production') {
+    app.use(logger('dev'));
+  }
+  app.use(cors());
+  app.use(helmet());
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({extended: true}));
+  app.use('/api/v1', routes.users);
 
-mongoose.connect(config.connUri, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
-    .catch(error => console.log(error));
-
-if (config.environment !== 'production') {
-  app.use(logger('dev'));
+  app.listen(config.port, () => {
+    console.log(`Server is running on ${config.baseUrl}:${config.port}/`);
+  });
 }
 
-app.use('/api/v1', routes.users);
+startServer();
 
-app.listen(config.port, () => {
-  console.log(`Server is running on ${config.baseUrl}:${config.port}/`);
-});
+
+
+
+
