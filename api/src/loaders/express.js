a040187f516664adb4b30887f44b7f7e12c46328
Th <tharathorn.bu@ku.th>
2021-05-18 00:45:09 +0700

    Add dependencies from loaders

--- api/src/loaders/express.js
+++ api/src/loaders/express.js
@@ -2,16 +2,14 @@ const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
 const path = require("path");
-const flash = require("connect-flash");
 const appRootPath = require("app-root-path");
 
 module.exports = async () => {
     const app = express();
     app.use(helmet());
     app.set("trust proxy", 1);
-    app.set("views", path.join(appRootPath.toString(), "src/views"));
-    app.set("view engine", "ejs");
+    // app.set("views", path.join(appRootPath.toString(), "src/views"));
+    // app.set("view engine", "ejs");
     app.use(cors());
-    app.use(flash());
     return app;
 };
