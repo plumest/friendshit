9e79d2093f277734303798f889b3b48ce5f05695
Th <tharathorn.bu@ku.th>
2021-05-18 01:08:19 +0700

    Fix babel transpiler for ES modules

--- api/src/index.js
+++ api/src/index.js
@@ -1,25 +1,25 @@
-import config from './config.js'
-import cors from 'cors';
+import config from "./config.js";
+import cors from "cors";
 import helmet from "helmet";
-import logger from 'morgan';
-import express from 'express';
-import mongoose from 'mongoose';
-const load = require('loaders/index')
-import routes from './routes/index.js';
+import logger from "morgan";
+import express from "express";
+import mongoose from "mongoose";
+import load from "./loaders";
+import routes from "./routes/index.js";
 import bodyParser from "body-parser";
 
 let app = express();
 
 async function startServer() {
-  app = await load()
-  if (config.environment !== 'production') {
-    app.use(logger('dev'));
+  app = await load();
+  if (config.environment !== "production") {
+    app.use(logger("dev"));
   }
   app.use(cors());
   app.use(helmet());
   app.use(bodyParser.json());
-  app.use(bodyParser.urlencoded({extended: true}));
-  app.use('/api/v1', routes.users);
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use("/api/v1", routes.users);
 
   app.listen(config.port, () => {
     console.log(`Server is running on ${config.baseUrl}:${config.port}/`);
@@ -27,8 +27,3 @@ async function startServer() {
 }
 
 startServer();
-
-
-
-
-
