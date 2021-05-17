c1b77f7eb9dae0695d9d89eec265201e7ca62b7a
magmagcup <sirawich.d@ku.th>
2021-05-18 00:25:17 +0700

    Rename and finish api for book

--- api/controllers/books.js
+++
@@ -1,5 +0,0 @@
-import config from '../config.js'
-import mongoose from 'mongoose';
-import Book from '../models/books.js';
-
-export default {  };
---
+++ api/src/controllers/books.js
@@ -0,0 +1,83 @@
+import config from '../config.js'
+import mongoose from 'mongoose';
+import Book from '../models/books.js';
+
+const create = (req, res) => {
+    let result = {};
+    let status = 201;
+
+    const { owner, name } = req.body;
+    const notes = [];
+    const book = new Book({ owner, name, notes });
+
+    book.save()
+        .then(book => {
+            result.status = status;
+            result.result = book;
+        }).catch(err => {
+        console.log(err)
+        status = 500
+        result.status = 500;
+        result.error = err;
+    }).finally(() => {
+        res.status(status).send(result);
+    });
+}
+
+const getOneBook = (req, res) => {
+    const {id , owner} = req.body;
+
+    let result = {};
+    let status = 200;
+
+    Book.findOne({id}, {owner}, (err, book) => {
+         if (!err && book) {
+             result.status = status;
+             result.result = book;
+        } else {
+             status = 404;
+             result.status = status;
+             result.error = "Book not found"
+         }
+         res.status(status).send(result);
+    }).catch(err => {
+        status = 500;
+        result.status = status;
+        result.error = err;
+        res.status(status).send(result);
+    });
+}
+
+const updateBookPage = (req, res) => {
+    let result = {};
+    let status = 201;
+
+    const { owner, name, id ,note } = req.body;
+
+    Book.findOne({id}, {owner}, (err, book) => {
+        if (!err && book) {
+            book.update({$push: {notes : {note: note}}});
+            result.status = status;
+            result.result = book;
+        } else {
+            status = 404;
+            result.status = status;
+            result.error = "Book not found"
+        }
+        res.status(status).send(result);
+        book.save()
+        .then(book => {
+            result.status = status;
+            result.result = book;
+        }).catch(err => {
+        console.log(err)
+        status = 500
+        result.status = 500;
+        result.error = err;
+    }).finally(() => {
+        res.status(status).send(result);
+    });
+})
+}
+
+export default { create, getOneBook, updateBookPage };
