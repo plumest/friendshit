08c23cf61c4e3a7c0fd264bec62eb37342be8eac
plumest <plumest.chayathon@gmail.com>
2021-05-19 12:04:43 +0700

    Remove unnecessary options

--- api/src/utils.js
+++ api/src/utils.js
@@ -7,8 +7,7 @@ const validateToken = (req, res, next) => {
     if (authorizationHeaader) {
         const token = req.headers.authorization.split(' ')[1]; // Bearer <token>
         const options = {
-            expiresIn: '1d',
-            issuer: 'https://scotch.io'
+            expiresIn: '1d'
         };
         try {
             // verify makes sure that the token hasn't expired and has been issued by us
